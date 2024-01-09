7ff558a8cb9f647df768293a35cd45cce7d41e72
Tom Anderson <tom@ando.gq>
2024-01-09 20:14:17 +1100

    Get a consume API working

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ const STREAM_END = Symbol.for("STREAM_END");
 type StreamAtom<TValue> = TValue | typeof STREAM_END;
 type NextValueCallback<TValue> = (cb: (value: StreamAtom<TValue>) => void) => void;
 
+/**
+ * @param value - Value being emitted in the stream
+ * @param push - Callback function to emit a new value down the stream
+ */
+type ConsumeCallback<TValue, TValue_> = (value: StreamAtom<TValue>, push: (value: StreamAtom<TValue_>) => void) => void;
+
 class Stream<TValue> {
     get_next_value: NextValueCallback<TValue>;
 
@@ -10,15 +16,61 @@ class Stream<TValue> {
         this.get_next_value = next_value;
     }
 
+    next(cb: (value: StreamAtom<TValue>) => void) {
+        let emitted = false;
+
+        this.get_next_value((value) => {
+            if (emitted) {
+                console.error("value emitted twice");
+            } else {
+                emitted = true;
+                cb(value);
+            }
+        });
+    }
+
+    // De-dupe value callbacks
+    consume<TValue_>(consumer: ConsumeCallback<TValue, TValue_>): Stream<TValue_> {
+        // Must give consumer following arguments:
+        // - value: The last value emitted from the stream
+        // - push: A method to add a new value to the resulting stream (can be re-used)
+
+        let queue: Array<StreamAtom<TValue_>> = [];
+
+        return new Stream((provide_value) => {
+            // Make sure that queue has some values in it
+            if (queue.length === 0) {
+
+                // Get the value from upstream
+                this.next((value) => {
+                    // Build the push/next methods
+
+                    // Take value, and add it to the end of the queue
+                    const push = (value: StreamAtom<TValue_>) => {
+                        queue.push(value);
+                    };
+
+                    consumer(value, push);
+                });
+            }
+
+            if (queue.length > 0) {
+                // Value remains in queue, fetch it before continuing
+                return provide_value(queue.shift() as any);
+            } else {
+                console.warn("somehow ended up with an empty queue");
+                return provide_value(STREAM_END);
+            }
+        });
+    }
+
     map<TValue_>(op: (value: TValue) => TValue_): Stream<TValue_> {
-        return new Stream((cb) => {
-            this.get_next_value((value) => {
-                if (value === STREAM_END) {
-                    cb(STREAM_END);
-                } else {
-                    cb(op(value));
-                }
-            });
+        return this.consume((value, push, ) => {
+            if (value === STREAM_END) {
+                push(STREAM_END);
+            } else {
+                push(op(value));
+            }
         });
     }
 
@@ -26,7 +78,7 @@ class Stream<TValue> {
         return new Stream((cb) => {
             const then = Date.now();
 
-            this.get_next_value((value) => {
+            this.next((value) => {
                 const now = Date.now();
 
                 setTimeout(() => {
@@ -37,26 +89,24 @@ class Stream<TValue> {
     }
 
     [Symbol.asyncIterator]() {
-        return this;
-    }
-
-    next(): Promise<IteratorResult<TValue, void>> {
-        return new Promise((resolve) => {
-            this.get_next_value((value) => {
-                if (value === STREAM_END) {
-                    resolve({ done: true, value: undefined });
-                } else {
-                    resolve({ done: false, value });
-                }
-            });
-        });
+        return {
+            next: () => new Promise<IteratorResult<TValue, unknown>>((resolve) => {
+                this.next((value) => {
+                    if (value === STREAM_END) {
+                        resolve({ done: true, value: undefined });
+                    } else {
+                        resolve({ done: false, value });
+                    }
+                });
+            })
+        };
     }
 
     toArray(cb: (array: Array<TValue>) => void) {
         let array: Array<TValue> = [];
 
         const recurse = () => {
-            this.get_next_value((value) => {
+            this.next((value) => {
                 if (value === STREAM_END) {
                     cb(array);
                 } else {
@@ -84,18 +134,31 @@ class Stream<TValue> {
     }
 };
 
-console.log("running");
+async function run() {
+    console.log("running");
 
-let s = Stream.from([
-    1,
-    2,
-    3
-])
-    .delay(1000)
-    .map((value) => value.toString(10));
+    let s = Stream.from([
+        1,
+        2,
+        3
+    ])
+    // .delay(1000)
+    .consume((value, push) => {
+        if (value === STREAM_END) {
+            push(STREAM_END);
+        } else {
+            push(value);
+            push(value * 10);
+        }
+    })
+    // TODO: Fix inference
+    .map((value) => (value as number).toString(10));
 
-for await (let value of s) {
-    console.log(value);
+    for await (let value of s) {
+        console.log(value);
+    }
+
+    console.log("async done");
 }
 
-console.log("async done");
+run();
