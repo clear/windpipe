c2ab3acfc273a54c7dec3543c9cd90c6a7e9eb0c
Tom Anderson <tom@ando.gq>
2024-04-09 10:40:51 +1000

    export `$` to emulate highland's API

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export { Stream } from "./stream";
+import { Stream } from "./stream";
+import type { StreamBase } from "./stream/base";
 export * as atom from "./atom";
+
+export { Stream } from "./stream";
+
+// Attempt to emulate Highland API
+type HighlandConstructor = typeof StreamBase["from"] & { of: typeof StreamBase["of"] };
+export const $: HighlandConstructor = Stream.from as HighlandConstructor;
+$.of = Stream.of;
