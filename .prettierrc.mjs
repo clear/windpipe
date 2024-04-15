f787e18284dd26f1dea1153a3a72555e43468313
Tom Anderson <tom@ando.gq>
2024-04-15 12:04:44 +1000

    add eslint and prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,8 @@
+export default {
+    "useTabs": false,
+    "singleQuote": false,
+    "trailingComma": "all",
+    "printWidth": 100,
+    "semi": true,
+    "tabWidth": 4
+}
---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    ignores: [
+      "dist/*",
+      "docs/*",
+      ".github/*",
+      ".changeset/*",
+    ],
+  },
+
+  // Use node globals
+  { languageOptions: { globals: globals.node } },
+
+  // Use recommended JS config
+  pluginJs.configs.recommended,
+
+  // Use recommended TS config
+  ...tseslint.configs.recommended,
+
+  // Allow unused variables that start with _
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_",
+      }]
+    }
+  },
+
+  // Delegate to prettier as last plugin
+  eslintPluginPrettier,
+];
