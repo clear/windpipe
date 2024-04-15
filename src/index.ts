e76371ef875fc0f864b540e93eb26af040d74ed4
Tom Anderson <tom@ando.gq>
2024-04-15 16:27:58 +1000

    attempt to improve exported API

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
 import { Stream } from "./stream";
 import type { StreamBase } from "./stream/base";
-export * as atom from "./atom";
+import { ok, error, unknown, isOk, isError, isUnknown } from "./atom";
 export * from "./util";
 
 export { Stream, type StreamEnd } from "./stream";
 
 // Attempt to emulate Highland API
-type HighlandConstructor = (typeof StreamBase)["from"] & { of: (typeof StreamBase)["of"] };
+type HighlandConstructor = (typeof StreamBase)["from"] & {
+    of: (typeof StreamBase)["of"];
+
+    /**
+     * Create a stream with a single `ok` atom on it.
+     */
+    ok: <T, E>(value: T) => Stream<T, E>;
+
+    /**
+     * Create a stream with a single `error` atom on it.
+     */
+    error: <T, E>(value: E) => Stream<T, E>;
+
+    /**
+     * Create a stream with a single `unknown` atom on it.
+     */
+    unknown: <T, E>(value: unknown) => Stream<T, E>;
+};
 export const $: HighlandConstructor = Stream.from as HighlandConstructor;
 $.of = Stream.of;
+
+$.ok = (value) => Stream.of(ok(value));
+$.error = (value) => Stream.of(error(value));
+$.unknown = (value) => Stream.of(unknown(value, []));
+
+export const atom = {
+    ok,
+    error,
+    unknown,
+    isOk,
+    isError,
+    isUnknown,
+};
