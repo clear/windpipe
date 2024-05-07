224c819c4fb34781cf5047539bb0976ebd1ae87c
mdboon <mark.boon@clearxp.com>
2024-05-07 16:01:02 +1000

    Adds cachedFlatMap operator

--- test/higher-order.test.ts
+++ test/higher-order.test.ts
@@ -127,6 +127,111 @@ describe.concurrent("higher order streams", () => {
         });
     });
 
+    describe.concurrent("cachedFlatMap", () => {
+        test("lookup non-repeating strings returning single atom", async ({ expect }) => {
+            expect.assertions(2);
+
+            const lookup = vi.fn((param: string) => $.of(param));
+
+            const s = $.from(["a", "b", "c"]).cachedFlatMap(lookup, (v) => v);
+
+            expect(await s.toArray({ atoms: true })).toEqual([$.ok("a"), $.ok("b"), $.ok("c")]);
+            expect(lookup).toBeCalledTimes(3);
+        });
+
+        test("lookup repeating strings returning single atom", async ({ expect }) => {
+            expect.assertions(2);
+
+            const lookup = vi.fn((param: string) => $.of(param));
+
+            const s = $.from(["a", "b", "c", "a", "a"]).cachedFlatMap(lookup, (v) => v);
+
+            expect(await s.toArray({ atoms: true })).toEqual([
+                $.ok("a"),
+                $.ok("b"),
+                $.ok("c"),
+                $.ok("a"),
+                $.ok("a"),
+            ]);
+            expect(lookup).toBeCalledTimes(3);
+        });
+
+        test("lookup repeating numbers returning multiple atoms", async ({ expect }) => {
+            expect.assertions(2);
+
+            const lookup = vi.fn((n: number) => $.fromArray([n, n * 2, n * 4]));
+
+            const s = $.from([1, 100, 200, 1, 10]).cachedFlatMap(lookup, (v) => v);
+
+            expect(await s.toArray({ atoms: true })).toEqual([
+                $.ok(1),
+                $.ok(2),
+                $.ok(4),
+                $.ok(100),
+                $.ok(200),
+                $.ok(400),
+                $.ok(200),
+                $.ok(400),
+                $.ok(800),
+                $.ok(1),
+                $.ok(2),
+                $.ok(4),
+                $.ok(10),
+                $.ok(20),
+                $.ok(40),
+            ]);
+            expect(lookup).toBeCalledTimes(4);
+        });
+
+        test("lookup repeating numbers returning multiple atoms", async ({ expect }) => {
+            expect.assertions(2);
+
+            const oneHundredDividedBy = vi.fn((n: number) => {
+                if (n === 0) {
+                    throw "Cannot divide by zero!";
+                }
+
+                return $.of(100 / n);
+            });
+
+            const s = $.from([5, 0, 50, 5, 5]).cachedFlatMap(oneHundredDividedBy, (v) => v);
+
+            expect(await s.toArray({ atoms: true })).toEqual([
+                $.ok(20),
+                $.unknown("Cannot divide by zero!", ["cachedFlatMap"]),
+                $.ok(2),
+                $.ok(20),
+                $.ok(20),
+            ]);
+            expect(oneHundredDividedBy).toBeCalledTimes(3);
+        });
+
+        test("lookup repeating numbers, including an error, returning multiple atoms", async ({
+            expect,
+        }) => {
+            expect.assertions(2);
+
+            const lookup = vi.fn((n: number) => $.of(n));
+
+            const s = $.from<number, unknown>([
+                $.ok(1),
+                $.ok(2),
+                $.error("oh no!"),
+                $.ok(2),
+                $.ok(1),
+            ]).cachedFlatMap(lookup, (v) => v);
+
+            expect(await s.toArray({ atoms: true })).toEqual([
+                $.ok(1),
+                $.ok(2),
+                $.error("oh no!"),
+                $.ok(2),
+                $.ok(1),
+            ]);
+            expect(lookup).toBeCalledTimes(2);
+        });
+    });
+
     describe.concurrent("flatten", () => {
         test("simple nested stream", async ({ expect }) => {
             expect.assertions(1);
